ede749dc3a39fc5f2b4fd1f4c9560d87a688218d
exhaustion666 <crowford-1337@mail.ru>
2025-05-12 17:21:21 +0400

    Add files via upload
    final functions + final results

---
+++ lab_2/main.py
@@ -0,0 +1,16 @@
+from nist import *
+
+
+def main():
+    results = []
+    results.append(("Frequency Bitwise Test (C++)", freq_bitwise_test(CPP_SEQ)))
+    results.append(("Same Bits Test (C++)", same_bits_test(CPP_SEQ)))
+    results.append(("Longest Test (C++)", longest_test(CPP_SEQ)))
+    results.append(("Frequency Bitwise Test (Java)", freq_bitwise_test(JAVA_SEQ)))
+    results.append(("Same Bits Test (Java)", same_bits_test(JAVA_SEQ)))
+    results.append(("Longest Test (Java)", longest_test(JAVA_SEQ)))
+    write_results(results, "results.txt")
+
+
+if __name__ == "__main__":
+    main()
