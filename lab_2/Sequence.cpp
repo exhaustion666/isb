d9247180f8181b420a7cb3003afcd34c2023d247
exhaustion666 <crowford-1337@mail.ru>
2025-05-14 20:15:50 +0400

    Update Sequence.cpp
    added docstring

--- lab_2/Sequence.cpp
+++ lab_2/Sequence.cpp
@@ -3,6 +3,16 @@
 #include <cstdlib>
 #include <ctime>
 
+/**
+ * Generates a random binary sequence of length 128 bits
+ * and saves it to the file "cpp_seq.txt".
+ *
+ * This program initializes the random number generator using the current time
+ * and generates a sequence of 0 or 1.
+ * The generated sequence is written to a text file named "cpp_seq.txt".
+ *
+ * @return int Returns 0 upon successful completion of the program.
+ */
 int main() {
     std::srand(static_cast<unsigned int>(std::time(0)));
     std::ofstream outFile("cpp_seq.txt");
