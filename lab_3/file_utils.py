50323a6aa706b9df888c2fa2fd54feee66b90b75
exhaustion666 <crowford-1337@mail.ru>
2025-05-26 18:03:23 +0400

    added exception handling

--- lab_3/file_utils.py
+++ lab_3/file_utils.py
@@ -1,25 +1,65 @@
 import json
+import os
 
 def load_config(path: str) -> dict:
-    with open(path, 'r', encoding='utf-8') as f:
-        return json.load(f)
+    try:
+        with open(path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Configuration file not found at {path}.")
+    except json.JSONDecodeError:
+        raise ValueError(f"Error decoding JSON in file {path}. Check the format.")
+    except Exception as e:
+        raise RuntimeError(f"Error loading configuration: {e}")
 
 
 def load_text_file(path: str) -> str:
-    with open(path, 'r', encoding='utf-8') as f:
-        return f.read()
+    try:
+        with open(path, 'r', encoding='utf-8') as f:
+            return f.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Text file not found at {path}.")
+    except Exception as e:
+        raise RuntimeError(f"Error reading text file {path}: {e}")
 
 
 def save_text_file(path, content: str) -> None:
-    with open(path, 'w', encoding='utf-8') as f:
-        f.write(content)
+    try:
+        with open(path, 'w', encoding='utf-8') as f:
+            f.write(content)
+    except Exception as e:
+        raise RuntimeError(f"Error saving text file {path}: {e}")
 
 
 def load_binary(path: str) -> bytes:
-    with open(path, 'rb') as f:
-        return f.read()
+    try:
+        with open(path, 'rb') as f:
+            return f.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Binary file not found at {path}.")
+    except Exception as e:
+        raise RuntimeError(f"Error reading binary file {path}: {e}")
 
 
-def save_binary(path, content: str) -> None:
-    with open(path, 'wb') as f:
-        f.write(content)
+def save_binary(path, content: bytes) -> None:
+    try:
+        with open(path, 'wb') as f:
+            f.write(content)
+    except Exception as e:
+        raise RuntimeError(f"Error saving binary file {path}: {e}")
+
+
+def save_encrypted_file(path: str, content: bytes) -> None:
+    try:
+        with open(path, 'wb') as f:
+            f.write(content)
+    except Exception as e:
+        raise RuntimeError(f"Error saving encrypted file {path}: {e}")
+
+
+def save_decrypted_file(path: str, content: str) -> None:
+    try:
+        with open(path, 'w', encoding='utf-8') as f:
+            f.write(content)
+    except Exception as e:
+        raise RuntimeError(f"Error saving decrypted file {path}: {e}")
